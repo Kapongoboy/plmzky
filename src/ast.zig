1e0e3d99f9257dfe44cbbed202ef6efe55ddd8cd
Plamedi Luis <plamedi.luis11@gmail.com>
2024-07-05 10:03:38 +0200

    union enum approach

--- src/ast.zig
+++ src/ast.zig
@@ -1,22 +1,31 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
 const Token = @import("token.zig").Token;
 
-pub const Identifier = struct {
-    token: Token,
-    value: []const u8,
-
-    pub fn init(t: Token, v: *const []u8) Identifier {
-        return Identifier{
-            .token = t,
-            .value = v.*,
-        };
+const Node = union(enum) {
+    fn tokenLiteral() []const u8 {
+        @compileError("not yet implemented");
     }
+};
 
-    pub fn token(self: *Identifier) *const Token {
-        return &self.token;
+const Statement = union(enum) {
+    fn statementNode() Node {
+        @compileError("not yet implemented");
     }
+};
 
-    pub fn value(self: *Identifier) *const []const u8 {
-        &self.value;
+const Expression = union(enum) {
+    fn expressionNode() Node {
+        @compileError("not yet implemented");
+    }
+};
+
+const Program = struct {
+    statements: ArrayList(Statement),
+
+    pub fn tokenLiteral(p: *Program) []const u8 {
+        if (p.statements.items.len > 0) {
+            return p.statements.items[0].statementNode().tokenLiteral();
+        }
     }
 };
